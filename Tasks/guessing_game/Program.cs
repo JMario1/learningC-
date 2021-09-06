c7fab8b3f92aac399121dbbf908740b19b8345b8
JMario1 <jdonmarie@gmail.com>
2021-09-06 12:40:25 +0100

    Add guessing game and result roaster

---
+++ Tasks/guessing_game/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 0;
+            Random rng = new Random();
+            int guess = 0;
+            Console.WriteLine("Welcome, want to play a game!!!");
+            Console.WriteLine("Start by picking a mode. \n Easy: guess a number between 1 and 10, you got 6 tries. Press 1 to select  \n Intermediate: guess a number between 1 and 20, you got 4 tries. Press 2 to select \n Hard: guess a number between 1 and 50, you got 3 tries. Press 3 to select. \n");
+            Console.Write("Choose mode: ");
+            level = int.Parse(Console.ReadLine());
+            if(level == 1)
+            {
+                int number = rng.Next(1, 10);
+                for(int i =1; i <= 6; i++)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = int.Parse(Console.ReadLine());
+                    if(guess == number)
+                    {
+                        Console.WriteLine("Congratulation!!! \n You won");
+                        break;
+                    }
+                    else if( i == 6)
+                    {
+                        Console.WriteLine("You lost, better luck next time");
+                        Console.WriteLine("Correct answer was " + number );
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again");
+                        continue;
+                    }
+                }
+                
+            }
+            else if(level == 2)
+            {
+                int number = rng.Next(1, 10);
+                for(int i =1; i <= 4; i++)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = int.Parse(Console.ReadLine());
+                    if(guess == number)
+                    {
+                        Console.WriteLine("Congratulation!!! \n You won");
+                        break;
+                    }
+                    else if( i == 4)
+                    {
+                        Console.WriteLine("You lost, better luck next time");
+                        Console.WriteLine("Correct answer was "+ number );
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again");
+                        continue;
+                    }
+                }
+            }
+            else if(level == 3)
+            {
+                int number = rng.Next(1, 10);
+                for(int i =1; i <= 3; i++)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = int.Parse(Console.ReadLine());
+                    if(guess == number)
+                    {
+                        Console.WriteLine("Congratulation!!! \n You won");
+                        break;
+                    }
+                    else if( i == 3)
+                    {
+                        Console.WriteLine("You lost, better luck next time");
+                        Console.WriteLine("Correct answer was "+ number );
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again");
+                        continue;
+                    }
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ Tasks/result_roaster/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace result_roaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int numberOfStudents = rng.Next(50, 70);
+            int passed = 0;
+            int average = 0;
+            int failed = 0;
+            int[] grades = new int[numberOfStudents];
+            for(int i =0; i <numberOfStudents; i++)
+            {
+                int grade = rng.Next(1,100);
+                grades[i] = grade;
+
+                if(grade >= 70)
+                {
+                    passed += 1;
+                }
+                else if(grade >=50 )
+                {
+                    average += 1;
+                }
+                else
+                {
+                    failed += 1;
+                }
+            }
+            Console.WriteLine("There are " + numberOfStudents + " students in this class");
+            Console.WriteLine(passed + " students passed with 70 and above");
+            Console.WriteLine(average + " students had between 50 amd 69");
+            Console.WriteLine(failed + " students failed");
+            for(int i=0; i<numberOfStudents; i++)
+            {
+                Console.Write(grades[i]+ ", ");
+            }            
+        }
+    }
+}
